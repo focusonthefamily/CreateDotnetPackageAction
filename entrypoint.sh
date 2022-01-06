ae06d11f785d78087e0e05deb75d16a7965fd6d6
Matt Pell <matt.pell@fotf.org>
2022-01-06 16:37:17 -0700

    use working directory for base find directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -euxo pipefail
 workingDirectory=$(pwd)
-cd $(find . -type f -name '*.sln' | dirname xargs-0)
+cd $(find $workingDirectory -type f -name '*.sln' | dirname xargs-0)
 dotnet restore
 dotnet test
 dotnet publish --no-restore -r linux-x64 --configuration Release --self-contained true
-cd $(find . -type d -name 'publish' -path "*$1*")
+cd $(find $workingDirectory -type d -name 'publish' -path "*$1*")
 zip -r release.zip .
 mv release.zip $workingDirectory/release.zip
 cd $workingDirectory
