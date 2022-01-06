a55ab80e9cadf5256d2aa1972ac8d8e341c4e257
Matt Pell <matt.pell@fotf.org>
2022-01-06 16:42:46 -0700

    try this

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ cd $(find $workingDirectory -type f -name '*.sln' | dirname xargs-0)
 dotnet restore
 dotnet test
 dotnet publish --no-restore -r linux-x64 --configuration Release --self-contained true
-cd $(find $workingDirectory -type d -name 'publish' -path "*$1*")
+cd $(find $workingDirectory -type d -name 'publish' -path *$1*)
 zip -r release.zip .
 mv release.zip $workingDirectory/release.zip
 cd $workingDirectory
