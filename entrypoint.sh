3b89fc549155ef7282a9b7ce4ad75f6a974bd4d6
Matt Pell <matt.pell@fotf.org>
2022-01-07 10:10:54 -0700

    remove test verbosity

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ set -euxo pipefail
 workingDirectory=$(pwd)
 cd $(find $workingDirectory -type f -name '*.sln' | dirname xargs-0)
 dotnet restore
-dotnet test -v n
+dotnet test
 dotnet publish --no-restore -r linux-x64 --configuration Release --self-contained true
 publishDirectory=$(find $workingDirectory -type d -name 'publish' -path "*${1}*")
 if [ -z $publishDirectory ]; then
