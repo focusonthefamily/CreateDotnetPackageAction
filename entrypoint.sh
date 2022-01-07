4e02711b12c22277b49ca3d22f6cf162a8a4aab4
Matt Pell <matt.pell@fotf.org>
2022-01-07 09:35:44 -0700

    add checks for finding directories

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 set -euxo pipefail
 workingDirectory=$(pwd)
-cd $(find $workingDirectory -type f -name '*.sln' | dirname xargs-0)
+slnDirectory=$(find $workingDirectory -type f -name '*.sln' | dirname xargs-0)
+if [ -n $slnDirectory ]; then
+    cd $slnDirectory
+fi
 dotnet restore
 dotnet test
 dotnet publish --no-restore -r linux-x64 --configuration Release --self-contained true
-cd $(find $workingDirectory -type d -name 'publish' -path "*${1}*")
+publishDirectory=$(find $workingDirectory -type d -name 'publish' -path "*${1}*")
+if [ -z $publishDirectory ]; then
+    echo "no publish directory found, cancelling package creation"
+    exit 1;
+fi
+
+cd $publishDirctory
 zip -r release.zip .
 mv release.zip $workingDirectory/release.zip
 cd $workingDirectory
